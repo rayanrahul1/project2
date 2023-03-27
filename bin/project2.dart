f1fa22f64d6d40f54b273097145d253c0128027a
rayanrahul1 <rayanrahul100@gmail.com>
2023-03-27 11:29:10 +0530

    2

--- bin/project2.dart
+++ bin/project2.dart
@@ -1,18 +1,18 @@
-void main(){
+void main() {
   /// variable declaration
   /// datatype variable_name = value;
-   String name = "Rahul";
-   int age = 24;
-   int phone = 7012263463;
-   double mark = 6.6;
-   String email = "rayanrahul100@gmail.com";
+  String name = "Rahul";
+  int age = 24;
+  int phone = 7012263463;
+  double mark = 6.6;
+  String email = "rayanrahul100@gmail.com";
+  String college = "luminar";
 
-
-   print("student details");
-   print("my name is $name");
+  print("student details");
+  print("my name is $name");
   print("i am $age yrs old");
-   print("my phone number is $phone");
-   print("my mark is $mark");
-   print("my email is $email");
-
+  print("my phone number is $phone");
+  print("my mark is $mark");
+  print("my email is $email");
+  print("my college name $college");
 }
